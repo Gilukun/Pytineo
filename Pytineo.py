ed69ea049f734911b70d479fcdfee363db0d7f91
Gilukun <89963499+Gilukun@users.noreply.github.com>
2022-01-21 14:50:13 +0400

    Update Pytineo.py

--- Pytineo.py
+++ Pytineo.py
@@ -39,7 +39,7 @@ from PIL import Image
 
 #affichage de la page sur toute sa largeur. Ce code doit toujour être le premier à être entré après l'import des modules
 st.set_page_config(layout="wide")
-
+image = Image.open('https://github.com/Gilukun/Pytineo/blob/main/Pytineo_Logo_2.png')
 st.sidebar.image("Gilukun/Pytineo/blob/main/Pytineo_Logo_2.png", width=100)
 #creation de la navigation du site (menu de gauche)
 sidebar = st.sidebar.radio("Navigation", ["Acceuil", "Analyse de données", "Application Pytineo"]) 
